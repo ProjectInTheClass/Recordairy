73a2ed31c7c1ec006de64aa91f3192051c7609eb
naneunmina <mina061302@gmail.com>
2024-10-29 13:11:45 +0900

    initial setting

---
+++ Recordiary/RecordiaryTests/RecordiaryTests.swift
@@ -0,0 +1,16 @@
+//
+//  RecordiaryTests.swift
+//  RecordiaryTests
+//
+//  Created by 김민아 on 10/29/24.
+//
+
+import Testing
+
+struct RecordiaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Recordiary/RecordiaryUITests/RecordiaryUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  RecordiaryUITestsLaunchTests.swift
+//  RecordiaryUITests
+//
+//  Created by 김민아 on 10/29/24.
+//
+
+import XCTest
+
+final class RecordiaryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
