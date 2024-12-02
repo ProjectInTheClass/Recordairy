83baa0d86e1ab8d45064cd5e919c10c9abac540f
2022014557 <2022014557@hanyang.ac.kr>
2024-12-02 18:36:15 +0900

    수정2

--- Recordiary/Recordiary/Views/MainView.swift
+++ Recordiary/Recordiary/Views/MainView.swift
@@ -75,23 +75,3 @@ struct MainView: View {
     }
 }
 
-struct CustomModal<Content: View>: View {
-    let content: Content
-
-    init(@ViewBuilder content: () -> Content) {
-        self.content = content()
-    }
-
-    var body: some View {
-        VStack(spacing: 16) {
-            // 상단 그랩바
-            RoundedRectangle(cornerRadius: 2.5)
-                .fill(Color(hex: "#E0E0E0"))
-                .frame(width: 39, height: 5)
-                .padding(.top, 8)
-            content
-        }
-        .background(Color(hex: "#FFFDF7").ignoresSafeArea()) // 공통 배경색
-        .presentationDetents([.medium, .large]) // 중형 & 대형 디텐트
-    }
-}
