7ef57081fbb6eb1ce37d1152d2adc6aea0a4efb3
RulerOfCakes <fragmentbombs@gmail.com>
2024-12-10 13:13:48 +0900

    fix: change default file extension to m4a

--- backend/src/utils.rs
+++ backend/src/utils.rs
@@ -6,6 +6,6 @@ pub mod parse_multipart;
 pub mod sqlx;
 
 pub fn get_diary_filename(user_id: Uuid, diary_id: i64) -> String {
-    // TODO: check file extension?
-    user_id.to_string() + "_" + &diary_id.to_string() + ".mp3"
+    // IOS recording only supports m4a
+    user_id.to_string() + "_" + &diary_id.to_string() + ".m4a"
 }
